67396f4b637100137ffefe61017f12fabdb5f2f4
Salamander333 <y.glushkov@abv.bg>
2020-07-02 16:06:45 +0300

    init

---
+++ MarketStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarketStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
